d81f99d23c2891df37b184a08d0c72d19faba58b
tanglefast23 <hoodotrixxy@gmail.com>
2026-06-23 17:48:50 +0700

    fix: suppress extension body hydration warning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
-      <body>
+      <body suppressHydrationWarning>
         <PlotGoblinMascot />
         {children}
       </body>
